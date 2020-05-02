8ca64734b75345d116867c70f5d85c503b462b1d
tahsinrahman <mdtahsinrahman@airasia.com>
2020-05-02 21:08:29 +0800

    Create private subnet and attach nat to it

--- modules/gcp/network/main.tf
+++ modules/gcp/network/main.tf
@@ -21,8 +21,11 @@ locals {
   # In project name format `<org>-<project>-<env>`, project_name_parts[2]
   # and project_name_parts[1] contains env and project name respectively
   # ---------------------------------------------------------------------
-  vpc_name    = format("%s-%s-%s%s", local.project_name_parts[2], local.project_name_parts[1], "vpc", local.suffix)
-  subnet_name = format("%s-%s-%s%s", local.project_name_parts[2], local.project_name_parts[1], "subnet", local.suffix)
+  vpc_name            = format("%s-%s-%s%s", local.project_name_parts[2], local.project_name_parts[1], "vpc", local.suffix)
+  public_subnet_name  = format("%s-%s-%s%s", local.project_name_parts[2], local.project_name_parts[1], "subnet-public", local.suffix)
+  private_subnet_name = format("%s-%s-%s%s", local.project_name_parts[2], local.project_name_parts[1], "subnet-private", local.suffix)
+  cloud_nat_name      = format("%s-%s-%s%s", local.project_name_parts[2], local.project_name_parts[1], "cloud-nat", local.suffix)
+  cloud_router_name   = format("%s-%s-%s%s", local.project_name_parts[2], local.project_name_parts[1], "cloud-router", local.suffix)
 }
 
 resource "google_compute_network" "vpc" {
@@ -36,8 +39,8 @@ resource "google_compute_network" "vpc" {
   routing_mode = "REGIONAL"
 }
 
-resource "google_compute_subnetwork" "vpc_subnet" {
-  name = local.subnet_name
+resource "google_compute_subnetwork" "vpc_public_subnet" {
+  name = local.public_subnet_name
 
   project = var.gcp_project
   region  = var.region
@@ -48,10 +51,32 @@ resource "google_compute_subnetwork" "vpc_subnet" {
   # The IP address range of the VPC in CIDR notation. A prefix of /16 is recommended.
   ip_cidr_range = "10.0.0.0/16"
 
+  log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
+}
+
+resource "google_compute_subnetwork" "vpc_private_subnet" {
+  name = local.private_subnet_name
+
+  project = var.gcp_project
+  region  = var.region
+  network = google_compute_network.vpc.self_link
+
+  # Whether the VMs in this subnet can access Google services without assigned external IP addresses.
+  private_ip_google_access = true
+  # The IP address range of the VPC in CIDR notation. A prefix of /16 is recommended.
+  ip_cidr_range = "10.1.0.0/16"
+
   secondary_ip_range {
-    range_name = "private-services"
-    # The IP address range of the VPC's secondary address range in CIDR notation. A prefix of /16 is recommended.
-    ip_cidr_range = "10.1.0.0/16"
+    ip_cidr_range = "10.2.0.0/16"
+    range_name    = "pod-ip-cidr"
+  }
+  secondary_ip_range {
+    ip_cidr_range = "10.3.0.0/16"
+    range_name    = "service-ip-cidr"
   }
 
   log_config {
@@ -59,4 +84,24 @@ resource "google_compute_subnetwork" "vpc_subnet" {
     flow_sampling        = 0.5
     metadata             = "INCLUDE_ALL_METADATA"
   }
+}
+
+resource "google_compute_router" "cloud_router" {
+  name    = local.cloud_router_name
+  project = var.gcp_project
+  region  = var.region
+  network = google_compute_network.vpc.self_link
+}
+
+resource "google_compute_router_nat" "cloud_nat" {
+  name                               = local.cloud_nat_name
+  project                            = var.gcp_project
+  region                             = var.region
+  router                             = google_compute_router.cloud_router.name
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  subnetwork {
+    name                    = google_compute_subnetwork.vpc_private_subnet.self_link
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+  nat_ip_allocate_option = "AUTO_ONLY"
 }
--- modules/gcp/network/outputs.tf
+++ modules/gcp/network/outputs.tf
@@ -3,26 +3,48 @@ output "network" {
   value       = google_compute_network.vpc.self_link
 }
 
-output "subnet" {
+output "public_subnet" {
   description = "A reference (self_link) to the public subnetwork"
-  value       = google_compute_subnetwork.vpc_subnet.self_link
+  value       = google_compute_subnetwork.vpc_public_subnet.self_link
 }
 
-output "subnet_name" {
+output "private_subnet" {
+  description = "A reference (self_link) to the priavte subnetwork"
+  value       = google_compute_subnetwork.vpc_private_subnet.self_link
+}
+
+output "public_subnet_name" {
   description = "Name of the public subnetwork"
-  value       = google_compute_subnetwork.vpc_subnet.name
+  value       = google_compute_subnetwork.vpc_public_subnet.name
 }
 
-output "subnet_cidr_block" {
-  value = google_compute_subnetwork.vpc_subnet.ip_cidr_range
+output "private_subnet_name" {
+  description = "Name of the priavte subnetwork"
+  value       = google_compute_subnetwork.vpc_private_subnet.name
 }
 
-output "subnet_gateway" {
-  value = google_compute_subnetwork.vpc_subnet.gateway_address
+output "public_subnet_cidr_block" {
+  value = google_compute_subnetwork.vpc_public_subnet.ip_cidr_range
 }
 
-output "subnet_secondary_cidr_block" {
-  value = google_compute_subnetwork.vpc_subnet.secondary_ip_range.0.ip_cidr_range
+output "private_subnet_cidr_block" {
+  value = google_compute_subnetwork.vpc_private_subnet.ip_cidr_range
+}
+
+output "public_subnet_gateway" {
+  value = google_compute_subnetwork.vpc_public_subnet.gateway_address
+}
+
+output "private_subnet_gateway" {
+  value = google_compute_subnetwork.vpc_private_subnet.gateway_address
+}
+
+output "private_subnet_secondary_cidr_block" {
+  value = google_compute_subnetwork.vpc_private_subnet.secondary_ip_range.0.ip_cidr_range
+}
+
+output "private_subnet_secondary_range_name" {
+  value = google_compute_subnetwork.vpc_private_subnet.secondary_ip_range.0.range_name
 }
 
 output "subnet_secondary_range_name" {
